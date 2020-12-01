de2ece02710e6ee23877a1c21211fd24b7402090
Vishvendra Panwar <vishvendra@live.com>
2020-12-01 07:29:58 +0530

    Learn Python3 Hard way

---
+++ 001.Learn.python3.hard.way/001.First.Program.py
@@ -0,0 +1,4 @@
+print("Hello World")
+print("Hello Again!")
+print("I am starting programming practice again after 2012. when i was in my graduation.")
+print("Hope something good will come out of this.")
