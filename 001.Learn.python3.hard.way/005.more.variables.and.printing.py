fbda15b9969a8793b788d20358cff089cfe9d480
Vishvendra Panwar <vishvendra@live.com>
2020-12-27 06:01:37 +0530

    Add files via upload

---
+++ 001.Learn.python3.hard.way/005.more.variables.and.printing.py
@@ -0,0 +1,20 @@
+my_name = 'Vishvendra'
+my_age = 28
+my_height = 181 # CM
+my_weight = 110 # KG
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black and white'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} CM tall.")
+print(f"He's {my_weight} KG heavy.")
+print(f"Actually that's not too heavy.")
+print(f"Hr's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
