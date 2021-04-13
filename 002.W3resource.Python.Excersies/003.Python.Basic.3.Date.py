2f250e7477fc284c857e71ce016104a6284b2eba
Vishvendra <vishvendra@live.com>
2021-04-13 06:10:57 +0530

    Ex4

--- 002.W3resource.Python.Excersies/003.Python.Basic.3.Date.py
+++ 002.W3resource.Python.Excersies/003.Python.Basic.3.Date.py
@@ -1,3 +1,3 @@
-from datetime import date
-today = date.today()
+import datetime
+today = datetime.datetime.now()
 print("Date is :", today)
---
+++ 002.W3resource.Python.Excersies/004.Python.Basic.4.circlearea.py
@@ -0,0 +1,5 @@
+from math import pi
+r = float(input("Enter circle radius:\n"))
+area = float(pi*r**2)
+print("The area of circle:\n")
+print(area)
