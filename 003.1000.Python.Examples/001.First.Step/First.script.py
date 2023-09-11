9da83325ab31a0857fd106994b493c73ac044099
Vishvendra-panwar <vishvendra@live.com>
2023-09-11 20:36:27 +0530

    11-Sep-2023

---
+++ 003.1000.Python.Examples/001.First.Step/First.script.py
@@ -0,0 +1,38 @@
+print ("hello World")
+
+print ("Hello") 
+#Comments for other developers
+
+print ("world") # More Comments
+
+# print ("This is not Printed")
+
+# Variables
+greeting = "Hello World!!!!!"
+print (greeting)
+
+print ("Value Types in python")
+print ( type (23) ) # int
+print ( type (3.14) ) # float
+print ( type ("hello") ) # str
+print ( type ("23")) # str
+print ( type ("3.14")) # str
+print ( type (None)) # NoneType
+print ( type (True)) # boole
+print ( type (False)) # bool
+
+print ( type ([])) # list
+print ( type ({})) # dict
+
+# print ( type (hello)) # NameError: name 'hello' is not defined
+print ( "Still running")
+
+print ("Floating Point Limitation")
+print (0.1 + 0.2)
+
+rectangle_width = 23
+rectangle_height = 100
+rectangle_area = (rectangle_width*rectangle_height)
+rectangle_circumference = (rectangle_height + rectangle_width) * 2
+print ("Rectangle Ares", rectangle_area)
+print ("Rectangle Circumference", rectangle_circumference)
