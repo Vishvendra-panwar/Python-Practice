5594706753e1922111208184df5d6ed9e7787250
Vishvendra-panwar <vishvendra@live.com>
2023-09-12 20:34:16 +0530

    12-Sep-2023

---
+++ 003.1000.Python.Examples/001.First.Step/Second.Step.py
@@ -0,0 +1,14 @@
+# We will discuss modules in this step
+import sys
+
+print ( sys.executable)
+print ( sys.platform )
+print ( sys.argv[0])
+print ( sys.version_info.major)
+print ( sys.getsizeof(1))
+print ( sys.getsizeof(42))
+print ( sys.getsizeof(1.0))
+print ( sys.getsizeof(""))
+print ( sys.getsizeof("a"))
+print ( sys.getsizeof("ab"))
+print ( sys.getsizeof("abdsdgsdgsdg"))
