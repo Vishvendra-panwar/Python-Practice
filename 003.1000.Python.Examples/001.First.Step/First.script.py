9b95e999af67cbfa914c5ce5bba59854e02b807b
Vishvendra-panwar <vishvendra@live.com>
2023-09-12 20:25:41 +0530

    12-Sep-2023

--- 003.1000.Python.Examples/001.First.Step/First.script.py
+++ 003.1000.Python.Examples/001.First.Step/First.script.py
@@ -30,9 +30,24 @@ print ( "Still running")
 print ("Floating Point Limitation")
 print (0.1 + 0.2)
 
+## rectangle Area & Circumference
+
 rectangle_width = 23
 rectangle_height = 100
 rectangle_area = (rectangle_width*rectangle_height)
 rectangle_circumference = (rectangle_height + rectangle_width) * 2
 print ("Rectangle Ares", rectangle_area)
 print ("Rectangle Circumference", rectangle_circumference)
+
+# Circle Area & Circumference
+
+radius = 7
+circle_circumference = 2*3.14*radius
+print ("Circle circumference is ", circle_circumference)
+print ("Circle area is ", 3.14*3.14*radius)
+
+
+import math
+
+print ("Circle area with math pi ", radius*radius*math.pi)
+print ("The Circumference is ", 2*radius*math.pi)
