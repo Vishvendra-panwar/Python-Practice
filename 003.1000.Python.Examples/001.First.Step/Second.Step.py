bd3531943a09c76a4abf0ae5f6cec05f1a88f325
Vishvendra-panwar <vishvendra@live.com>
2023-10-04 16:39:53 +0530

    04-Oct-2023

--- 003.1000.Python.Examples/001.First.Step/Second.Step.py
+++ 003.1000.Python.Examples/001.First.Step/Second.Step.py
@@ -1,14 +1,33 @@
 # We will discuss modules in this step
 import sys
 
-print ( sys.executable)
-print ( sys.platform )
-print ( sys.argv[0])
-print ( sys.version_info.major)
-print ( sys.getsizeof(1))
-print ( sys.getsizeof(42))
-print ( sys.getsizeof(1.0))
-print ( sys.getsizeof(""))
-print ( sys.getsizeof("a"))
-print ( sys.getsizeof("ab"))
-print ( sys.getsizeof("abdsdgsdgsdg"))
+print ( "sys.executable = ", sys.executable)
+print ( "sys.platform = ", sys.platform )
+print ( "sys.argv [0] = ", sys.argv[0])
+print ( "sys.version_info.manjor = ", sys.version_info.major)
+print ( "sys.getsizeof (1)", sys.getsizeof(1))
+print ( "sys.getsizeof (42)", sys.getsizeof(42))
+print ( "sys.getsizeof(1.0)", sys.getsizeof(1.0))
+print ( "sys.getsizeof("")", sys.getsizeof(""))
+print ( "sys.getsizeof(\"a\")", sys.getsizeof("a"))
+print ( "sys.getsizeof(\"ab\")", sys.getsizeof("ab"))
+print ( "sys.getsizeof(\"Vishvendra\")", sys.getsizeof("Vishvendra"))
+
+print ("hello")
+print ("world")
+print ("Foo", "Bar",)
+
+sys.stdout.write("hello")
+sys.stdout.write("world")
+
+### Promting for user input in Python 2
+
+def main():
+    print ("We have a question!")
+    name = input('Your name: ')
+    print("Hello " + name + ", how are you?")
+    a = input('First number: ')
+    b = input("Second number: ")
+    print (a + b)
+    print (int(a) + int (b))
+main()
